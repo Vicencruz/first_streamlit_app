444d05bf91888d19c0d50342d93317ad64e00dbf
Vicente Cruz <109280410+Vicencruz@users.noreply.github.com>
2022-07-14 11:12:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
 
-streanlit.header('Breaksfast Menu')
+streamlit.header('Breaksfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
